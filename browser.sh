4e99954118018a27b4270da50b418f367d348f63
Xeon Zolt <xeon.harsh@gmail.com>
2017-07-22 04:31:51 +0530

    firefox fix

--- browser.sh
+++ browser.sh
@@ -2,10 +2,8 @@
 
 set -eu		
 # firefox
-preferences_file="`echo /etc/skel/.mozilla/firefox/a.default/user.js`"
+preferences_file="`echo /home/hotelos/.mozilla/firefox/a.default/user.js`"
 if [ -f "$preferences_file" ]
 then
     echo "user_pref(\"browser.startup.homepage\", \"${event_url}\");" >> $preferences_file
 fi
-#chromium
-
