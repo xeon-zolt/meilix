dac9f05de480aabfc6ad9d83e090454687abdf0d
Harsh Lathwal <xeon.harsh@gmail.com>
2017-07-22 11:31:39 +0530

    chromium

--- browser.sh
+++ browser.sh
@@ -7,3 +7,6 @@ if [ -f "$preferences_file" ]
 then
     echo "user_pref(\"browser.startup.homepage\", \"${event_url}\");" >> $preferences_file
 fi
+
+#chromium
+change-homepage(){ sed -ri 's|( "${event_url}": ").*(",)|\1'"$@"'\2|' /home/hotelos/.config/chromium/Default/Preferences; }
